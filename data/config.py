51625661cd082f88926a39a66144f54fc25e3a62
Muslimbek <48023644+muslimbek77@users.noreply.github.com>
2022-09-08 21:40:53 +0500

    admin

--- data/config.py
+++ data/config.py
@@ -21,6 +21,8 @@ import os
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = str(os.environ.get("BOT_TOKEN"))  # Bot token
 ADMINS = list(os.environ.get("ADMINS"))  # adminlar ro'yxati
+print(ADMINS)
+print(type(ADMINS))
 IP = str(os.environ.get("ip"))  # Xosting ip manzili
 PROVIDER_TOKEN = str(os.environ.get("PROVIDER_TOKEN"))
 
